c9840aec73ddf3178d5ccd26c8883e9b35b189f9
vasokirvalidze <vasokirvalidze8@gmail.com>
2024-09-30 09:38:09 +0400

    Update main.py

--- homework1/main.py
+++ homework1/main.py
@@ -1,7 +1,18 @@
 def main() -> None:
     # Write your solution here
     pass
-
+    print("Hello World")
+    print("5 + 7 = ",5+7)
+    print("37 - 8 = ",37-8)
+    print("-90 * 4 = ",-90*4,)
+    print("100 / 3 = ",100/3)
+    print("0.2 + 0.1 = ",0.2+0.1)
+    print("7.068 * 4.3 = ",7.068*4.3)
+    print("8 ** 2 = ",8**2)
+    print("9 ** 3 = ",9**3)
+    print("5 / 3 = ",5/3)
+    print("5 // 3 = ",5//3)
+    print("99 % 7 = ",99%7)
 
 if __name__ == '__main__':
     main()
